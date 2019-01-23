315f2a3fa0588072620a9fd0ced94d7580cf68e4
Subodh Yadav <subodhy@exzeo.com>
2019-01-23 17:05:34 +0530

    Initial commit

---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+#provision.sh
+ sudo ucf --purge /boot/grub/menu.lst
+ sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+ sudo apt-get update
+ sudo UCF_FORCE_CONFFNEW=YES apt-get upgrade -y     # ucf --purge and UCF_FORCE_CONFFNEW is used to avoid the grub question
+ echo "apt-get update done."
+ sudo apt-get install -y python-dev python-pip
+ sudo pip install ansible
+ sudo timedatectl set-timezone Asia/Kolkata
+ sudo localectl set-locale LANG=en_US.utf8
+ sudo wget "https://s3.amazonaws.com/ansiblepackersubodhbucket/i_playbook.yml"
+ echo "Running Ansible."
+ sudo ansible-playbook ./i_playbook.yml
